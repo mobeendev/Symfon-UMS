8e1d32c75de98b2a7d344fd5ce7b0fc24e417a91
Abdul Mobeen <mobeendev@gmail.com>
2022-05-27 13:07:06 +0300

    updated front layout

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -15,21 +15,37 @@ use App\Repository\BookRepository;
 use App\Repository\CountryRepository;
 use App\Repository\DepartmentRepository;
 use App\Repository\TagRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
 /**
  * @Route("/admin", name="admin_")
+ * @IsGranted("ROLE_ADMIN")
  */
 class AdminController extends BaseController
 {
+    private Security $security;
+
+    public function __construct(UserRepository $userRepository , Security $security)
+    {
+        $this->userRepository = $userRepository;
+        $this->security = $security;
+
+    }
     /**
      * @Route("/", name="index")
      */
    public function index(): Response
     {
+//        dd($this->getUser()->getRoles());
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -5,21 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
- * @Route("app", name="app_")
+ * @Route("/app", name="app_")
+ * @IsGranted("ROLE_USER")
  */
 class FrontController extends AbstractController
 {
-    /**
-     * @Route("/", name="index")
-     */
-    public function index(): Response
-    {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
-        ]);
-    }
 
     /**
      * @Route("/home", name="home")
@@ -27,7 +20,19 @@ class FrontController extends AbstractController
     public function home(): Response
     {
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+                'controller_name' => 'FrontController',
         ]);
     }
+
+//    /**
+//     * @Route("/", name="index")
+//     */
+//    public function index(): Response
+//    {
+//        return $this->render('front/home.html.twig', [
+//            'controller_name' => 'FrontController',
+//        ]);
+//    }
+
+
 }
